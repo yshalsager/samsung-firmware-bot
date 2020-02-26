7407cb48bff59a382310303629b27e4fab185756
Enes Sastim <sastimenes@gmail.com>
2020-02-26 23:36:35 +0100

    fix wine installation

--- SamFirm/install_wine.sh
+++ SamFirm/install_wine.sh
@@ -2,5 +2,7 @@
 sudo dpkg --add-architecture i386 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
+sudo apt-get install software-properties-common
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 sudo apt-get update
 sudo apt install --install-recommends winehq-stable
