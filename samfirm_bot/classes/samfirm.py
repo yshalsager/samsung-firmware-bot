0c0abd001c082bfe934053487f72bef96ce1a65b
yshalsager <ysh-alsager@hotmail.com>
2020-02-28 20:56:46 +0200

    samfirm: improve date extraction
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- samfirm_bot/classes/samfirm.py
+++ samfirm_bot/classes/samfirm.py
@@ -74,7 +74,7 @@ class SamFirm:
         version = re.search(r"(?:Version: )(.*)", output).group(1)
         android_version = re.search(r"(?:OS: )(.*)", output).group(1).replace('(', ' (')
         filename = re.search(r"(?:Filename: )(.*)", output).group(1)
-        date = datetime.strptime(filename.split('_')[2][:8], '%Y%m%d').strftime('%Y-%m-%d')
+        date = datetime.strptime(re.search(r"(20[0-9]{6})", filename).group(1), '%Y%m%d').strftime('%Y-%m-%d')
         size = naturalsize(int(re.search(r"(?:Size: )(.*)(?: bytes)", output).group(1)))
         info = {"model": model, "system": version.split('/')[0],
                 "csc": version.split('/')[1], "bootloader": version.split('/')[2],
