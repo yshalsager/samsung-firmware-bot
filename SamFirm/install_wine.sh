dfcbfc58b5db347fd315b90d3628d3855055912c
Razvan <corsicanu22@gmail.com>
2020-06-17 13:07:08 +0300

    underp wine install
    this adds packages needed to make it work from a fresh 18.04 install

--- SamFirm/install_wine.sh
+++ SamFirm/install_wine.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 sudo dpkg --add-architecture i386 
+
+# https://github.com/lutris/docs/blob/master/WineDependencies.md
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 wine32 wine64
+sudo apt-get update
+
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 sudo apt-get install software-properties-common
